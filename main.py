adaec53ec5578f828a39be34917bd3af4e4a0223
Donat Szabo <szabodoni14@2784806726.kn.vutbr.cz>
2026-03-19 03:26:03 +0100

    Added real server stat tracking

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
 import psutil
 
-# This initializes your API
 app = FastAPI(title="Homelab Resource Monitor")
 
-# A basic health check endpoint
+
 @app.get("/")
-def health_check():
+def read_root():
     return {"status": "healthy", "message": "API is running!"}
 
-# The endpoint that actually does the work
+
 @app.get("/metrics")
-def get_metrics():
+def get_system_metrics():
+
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory().percent
+
+    disk_info = psutil.disk_usage('/')
+    disk_free_gb = round(disk_info.free / (1024 ** 3), 2)
+    disk_total_gb = round(disk_info.total / (1024 ** 3), 2)
+
     return {
-        "cpu_usage_percent": psutil.cpu_percent(interval=1),
-        "ram_usage_percent": psutil.virtual_memory().percent,
-        "disk_usage_percent": psutil.disk_usage('/').percent
+        "cpu_usage_percent": cpu,
+        "ram_usage_percent": ram,
+        "disk_free_gb": disk_free_gb,
+        "disk_total_gb": disk_total_gb
     }
